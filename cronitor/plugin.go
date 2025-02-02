1498ee7d38c1b37ebabc46dd7e25ffebab39e7a3
Asif Ali Shaik <itsmeshaikasifali@gmail.com>
2025-02-02 11:32:55 +0530

    feat(plugin): added the main and plugin function

--- cronitor/plugin.go
+++ cronitor/plugin.go
@@ -1,1 +1,14 @@
 package cronitor
+
+import (
+	"context"
+
+	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
+)
+
+func Plugin(ctx context.Context) *plugin.Plugin {
+	plugin := &plugin.Plugin{
+		Name: "steampipe-plugin-cronitor",
+	}
+	return plugin
+}
--- main.go
+++ main.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"github.com/SHAIKASIFALI/steampipe-plugin-cronitor/cronitor"
+	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
+)
+
+func main() {
+	plugin.Serve(&plugin.ServeOpts{
+		PluginFunc: cronitor.Plugin,
+	})
+}
